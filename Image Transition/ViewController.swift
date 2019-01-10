6cb53ddb1a7f16496361ac76fcb997cb19370cb0
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-10 20:51:20 +0530

    Curl Page Animation

--- Image Transition/ViewController.swift
+++ Image Transition/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var segmentControl: UISegmentedControl!
+    
+    let imageArray = ["food","nature","sports"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func segmentControlChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            updateImage(atIndex:0)
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            updateImage(atIndex:1)
+        }
+        else {
+            updateImage(atIndex:2)
+        }
+    }
+    
+    func updateImage(atIndex index:Int) {
+        UIView.transition(with: imageView, duration: 1, options: [.transitionCurlDown], animations: {
+            self.imageView.image = UIImage(named: self.imageArray[index])
+        })
+    }
+    
 }
 
